e001b59132d106d9e2f7a4599b4a343677793d5f
James Doran <jysdoran@gmail.com>
2023-06-05 17:47:36 +0100

    Add dataset script

---
+++ dataset.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+wget https://github.com/microsoft/CodeXGLUE/raw/main/Text-Code/NL-code-search-Adv/dataset.zip \
+&& unzip dataset.zip \
+&& cd dataset \
+&& wget https://zenodo.org/record/7857872/files/python.zip \
+&& unzip python.zip \
+&& python preprocess.py \
+&& rm -r python \
+&& rm -r *.pkl \
+&& rm -r *.txt \
+&& rm python.zip \
+&& cd ..
