b7dbbeb93533c4d40a692220e9439740d51428f6
James Doran <s2334723@uhtred.inf.ed.ac.uk>
2023-06-12 12:25:51 +0100

    Add MLP tuning scripts

--- finetune.sh
+++ finetune.sh
@@ -1,28 +1,35 @@
 lang=ruby
 #datasetdir=$HF_HOME/datasets/downloads/extracted/12f42e6cc1af7629398f6c47e7c7be2e772d82061287e04771732698b8e0e110/$lang/final/jsonl
 mkdir -p ./saved_models/$lang
-datasetdir=$SCRATCH/CodeXGLUE/Text-Code/NL-code-search-Adv/dataset
+#datasetdir=$SCRATCH/CodeXGLUE/Text-Code/NL-code-search-Adv/dataset
+export TRANSFORMERS_OFFLINE=1
+export WANDB_MODE=offline
+
+datasetdir=$SCRATCHBIG/dataset
+modeldir=$SCRATCHBIG/microsoft/codebert-base
 
 cd code
 python run.py \
     --output_dir=./saved_models/$lang \
     --model_type=roberta \
-    --config_name=microsoft/codebert-base \
-    --model_name_or_path=microsoft/codebert-base \
-    --tokenizer_name=roberta-base \
+    --config_name=$modeldir \
+    --model_name_or_path=$modeldir \
+    --tokenizer_name=$modeldir \
     --do_train \
-    --train_data_file=$datasetdir/train_small.jsonl \
-    --eval_data_file=$datasetdir/valid_small.jsonl \
+    --train_data_file=$datasetdir/train.jsonl \
+    --eval_data_file=$datasetdir/valid.jsonl \
     --test_data_file=$datasetdir/test.jsonl \
     --num_train_epochs 2 \
     --block_size 256 \
     --train_batch_size $1 \
-    --eval_batch_size 16 \
+    --eval_batch_size 64 \
     --learning_rate 5e-5 \
     --max_grad_norm 1.0 \
     --evaluate_during_training \
+    --gradient_checkpointing \
     --seed 123456 2>&1| tee train.log
 
+    # --tokenizer_name=roberta-base \
     # --train_batch_size 32 \
     # --train_data_file=../dataset/train.jsonl \
     # --eval_data_file=../dataset/valid.jsonl \
--- mlp_finetune.sh
+++ mlp_finetune.sh
@@ -1,6 +1,10 @@
-source ~/.bashrc
+#! /bin/bash
+eval "$(micromamba shell hook --shell=bash)"
 
-lang=python
+nvidia-smi -q
+
+batchsize=8
+lang=python_$batchsize
 #datasetdir=$HF_HOME/datasets/downloads/extracted/12f42e6cc1af7629398f6c47e7c7be2e772d82061287e04771732698b8e0e110/$lang/final/jsonl
 savedir=$SCRATCHBIG/saved_models/$lang
 mkdir -p $savedir
@@ -16,6 +20,7 @@ then
     bash copy_to_scratch.sh
 fi
 
+micromamba activate ML3.8
 
 cd code
 python run.py \
@@ -25,17 +30,18 @@ python run.py \
     --model_name_or_path=$modeldir \
     --tokenizer_name=$modeldir \
     --do_train \
+    --do_eval \
+    --do_test \
     --train_data_file=$datasetdir/train.jsonl \
     --eval_data_file=$datasetdir/valid.jsonl \
     --test_data_file=$datasetdir/test.jsonl \
     --num_train_epochs 2 \
     --block_size 256 \
-    --train_batch_size 64 \
-    --eval_batch_size 128 \
+    --train_batch_size $batchsize \
+    --eval_batch_size 64 \
     --learning_rate 5e-5 \
     --max_grad_norm 1.0 \
     --evaluate_during_training \
-    --gradient_checkpointing \
     --seed 123456 2>&1| tee train.log
 
     # --tokenizer_name=roberta-base \
@@ -47,5 +53,5 @@ python run.py \
     # --eval_data_file=$datasetdir/valid/${lang}_valid_0.jsonl.gz \
     # --test_data_file=$datasetdir/test/${lang}_test_0.jsonl.gz \
 mkdir -p ./saved_models/
-rm -r ./saved_models/$lang
-cp $savedir ./saved_models
+rm -rf ./saved_models/$lang
+cp -r $savedir ./saved_models
